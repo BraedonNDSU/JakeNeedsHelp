a268186cabfca9d74f816a36fea28cc0386cfd0f
Gaven Nelson <gaven.nelson@ndsu.edu>
2023-02-16 22:43:11 -0600

    Created UI outline from design docs

--- ndsu-scheels-2023/src/App.js
+++ ndsu-scheels-2023/src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-       This is our Scheels UI!
-
-        <br></br>
-        
-        Braedon's test commit
+      <div className="App-header"> Scheels Data Transfer Application</div>
+      <div className="App-body">
+        <div className="Body-prodTable">
+          <div className="ProdTable-search">Search By: <input type="text" id="prodTableSearch" /> </div>
+          <div className="ProdTable-body">
+           <div className = "ProdBody-header">Production Table </div>
+           <div className = "ProdBody-body">FAKE DATA</div>
+          </div>
+          <div className="ProdTable-submitButton">
+            <button>Submit</button>
+          </div>
+          <div className="ProdTable-resetButton">
+            <button>Reset</button>
+          </div>
+        </div>
+        <div className="Body-testTable">
+          <div className="TestTable-search">Search By: <input type="text" id="testTableSearch" /> </div>
+          <div className="TestTable-body">
+           <div className = "TestBody-header">Test Environment Table </div>
+           <div className = "TestBody-body">FAKE DATA</div>
+          </div>
+          <div className="TestTable-submitButton">
+            <button>Submit</button>
+          </div>
+          <div className="ProdTable-resetButton">
+            <button>Reset</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
